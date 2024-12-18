21f2dec7db793cb1fededc8bf25fd5db5d45ce16
ALudwig23 <2308313@student.toa.edu.my>
2024-12-19 03:19:14 +0800

    - incomplete parry function

---
+++ Assets/Scripts/Enemy/TestDummy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDummy : MonoBehaviour
+{
+    [SerializeField] private bool toggleSlash;
+    [SerializeField] private bool toggleThrust;
+    [SerializeField] private bool toggleParry;
+
+    [SerializeField] private Cooldown slashDuration;
+    [SerializeField] private Cooldown thrustDuration;
+    [SerializeField] private Cooldown attackCooldown;
+
+    [SerializeField] private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BasicSlash();
+    }
+
+    private void BasicSlash()
+    {
+        if (toggleSlash == false)
+        {
+            animator.Play("Idle");
+            return;
+        }
+
+        if (attackCooldown.CurrentProgress is Cooldown.Progress.Ready)
+        {
+            animator.Play("Slash");
+            attackCooldown.StartCooldown();
+        }
+        else if (attackCooldown.CurrentProgress is Cooldown.Progress.Finished)
+        {
+            attackCooldown.ResetCooldown();
+        }
+
+    }
+}
--- Assets/Scripts/Player/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -59,5 +59,6 @@ public class PlayerAnimationHandler : MonoBehaviour
         playerAnimator.SetBool("isDashing", playerMovement.IsDashing);
 
         playerAnimator.SetBool("isLowAttacking", playerCombat.LowAttack);
+        playerAnimator.SetBool("isParrying", playerCombat.Parrying);
     }
 }
