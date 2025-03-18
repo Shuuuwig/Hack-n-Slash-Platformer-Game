5537a5c9b9d7c4d74e84e3a50649becb0b8da93e
ALudwig23 <2308313@student.toa.edu.my>
2025-03-18 18:15:22 +0800

    chompy in progress

---
+++ Assets/Scripts/Enemy/Chompy/Chompy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chompy : EnemyClass
+{
+    [SerializeField] private Collider2D chompCollider;
+    [SerializeField] private Cooldown chompAttackDuration;
+    [SerializeField] private Cooldown chompAttackCooldown;
+
+    private bool neutralSlash;
+
+    public bool NeutralSlash { get { return neutralSlash; } }
+    public Collider2D ChompCollider
+    {
+        get { return chompCollider; }
+        set { chompCollider = value; }
+    }
+
+    protected override void EnemyMoveset()
+    {
+        if (canAttack == true && chompAttackDuration.CurrentProgress is Cooldown.Progress.Ready && chompAttackCooldown.CurrentProgress is Cooldown.Progress.Ready)
+        {
+            neutralSlash = true;
+            chompAttackDuration.StartCooldown();
+            Debug.Log("Slashing");
+        }
+
+        if (chompAttackDuration.CurrentProgress is Cooldown.Progress.Finished)
+        {
+            neutralSlash = false;
+            chompAttackCooldown.StartCooldown();
+            chompAttackDuration.ResetCooldown();
+        }
+
+        if (chompAttackCooldown.CurrentProgress is Cooldown.Progress.Finished)
+        {
+            chompAttackCooldown.ResetCooldown();
+        }
+    }
+
+    protected override void EnemyMovement()
+    {
+        base.EnemyMovement();
+
+        enemyRigidBody.velocity = new Vector2((target.position.x - transform.position.x) * speed, 0);
+    }
+}
--- Assets/Scripts/Enemy/HauntingBlade/HauntingBlade.cs
+++ Assets/Scripts/Enemy/HauntingBlade/HauntingBlade.cs
@@ -46,5 +46,6 @@ public class HauntingBlade : EnemyClass
         base.EnemyMovement();
 
         enemyRigidBody.velocity = new Vector2((target.position.x - transform.position.x) * speed, 0);
+        Debug.Log("Moving");
     }
 }
